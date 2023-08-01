31f1cd924b55702df266fd10369960af0caeb127
Anderson <andersonshermann@gmail.com>
2023-08-01 18:55:16 -0300

    Criação das sections projetos, experiência

--- js/script.js
+++ js/script.js
@@ -40,93 +40,45 @@ function verificaViewPort(){
 verificaViewPort();
 
 
-/*Transição das experiências e education*/
+/*Transição de cards*/
 function sobreMim(){
-    const divExperiencia = document.querySelectorAll('.experience-content div')
-    const liExperiencia = document.querySelectorAll('.experience-content ul li')
-    const divEducation = document.querySelectorAll('.education-content div')
-    const liEducation = document.querySelectorAll('.education-content ul li')
+    const experience = document.querySelectorAll('.experience-card')
+    const profile_card = document.querySelectorAll('.sigle-profile-card')
 
     function slideShow(index){
-        divExperiencia.forEach((div)=>{
-            div.classList.remove('ativo');
+        experience.forEach((div)=>{
+            div.classList.remove('active');
         });
-        liExperiencia.forEach((botao)=>{
-            botao.classList.remove('ativo');
+        profile_card.forEach((botao)=>{
+            botao.classList.remove('active');
         })
-        divExperiencia[index].classList.add('ativo');
-        liExperiencia[index].classList.add('ativo');
+        experience[index].classList.add('active');
+        profile_card[index].classList.add('active');
     }
     slideShow(0);
 
-    function slideShow2(index){
-        divEducation.forEach((div)=>{
-            div.classList.remove('ativo');
-        });
-        liEducation.forEach((botao)=>{
-            botao.classList.remove('ativo');
-        })
-        divEducation[index].classList.add('ativo');
-        liEducation[index].classList.add('ativo');
-    }
-    slideShow2(0);
+    // function slideShow2(index){
+    //     divEducation.forEach((div)=>{
+    //         div.classList.remove('ativo');
+    //     });
+    //     liEducation.forEach((botao)=>{
+    //         botao.classList.remove('ativo');
+    //     })
+    //     divEducation[index].classList.add('ativo');
+    //     liEducation[index].classList.add('ativo');
+    // }
+    // slideShow2(0);
 
-    liExperiencia.forEach((event,index)=>{
+    profile_card.forEach((event,index)=>{
         event.addEventListener('click', ()=>{
             slideShow(index)
         });
     });
 
-    liEducation.forEach((event, index)=>{
-        event.addEventListener('click', ()=>{
-            slideShow2(index)
-        });
-    });
+    // liEducation.forEach((event, index)=>{
+    //     event.addEventListener('click', ()=>{
+    //         slideShow2(index)
+    //     });
+    // });
 }
 sobreMim();
-
-
-const design_card_butttons = document.querySelectorAll('.design-card');
-const introduction_text = document.querySelectorAll('.introduction-text');
-
-const single_profile_card = document.querySelectorAll('.single-profile-card');
-const testimonial_card = document.querySelectorAll('.testimonial-card');
-
-design_card_butttons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        introduction_text.forEach((introduction, introductionIndex) => {
-            if (index === introductionIndex) {
-                introduction.style.display = 'block';
-            } else {
-                introduction.style.display = 'none';
-            }
-        });
-        design_card_butttons.forEach((btn, btnIndex) => {
-            if (index === btnIndex) {
-                btn.classList.add('active');
-            } else {
-                btn.classList.remove('active');
-                
-            }
-        });
-    });
-});
-
-single_profile_card.forEach((btn, index) => {
-    btn.addEventListener('click', ()=> {
-        testimonial_card.forEach((testimonialCard, testimonialCardIndex) => {
-            if (index === testimonialCardIndex) {
-                testimonialCard.style.display = 'block';
-            } else {
-                testimonialCard.style.display = 'none';
-            }
-        });
-        single_profile_card.forEach((cardBtn, cardIndex) => {
-            if (index === cardIndex) {
-                cardBtn.classList.add('profile-card-active');
-            } else {
-                cardBtn.classList.remove('profile-card-active');
-            }
-        });
-    });
-});
